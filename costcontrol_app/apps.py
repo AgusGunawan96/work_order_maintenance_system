a8f11c56b9a8ad7204302b344a0c98a06d3cdaa0
erelte <61399120+erelte@users.noreply.github.com>
2023-02-17 09:23:57 +0700

    Initial commit

---
+++ costcontrol_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CostcontrolAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'costcontrol_app'
---
+++ ppc_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PpcAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ppc_app'
