ce8d137dc30af794ae33dbe92fca53301a522f92
erelte <61399120+erelte@users.noreply.github.com>
2023-04-01 09:13:20 +0700

    Updated
    1. Accounting App
    2. WingoApp

--- accounting_app/admin.py
+++ accounting_app/admin.py
@@ -3,10 +3,29 @@ from accounting_app.models import cashPaymentBalance, cashPayment, cashPaymentAp
 import datetime
 
 # Register your models here.
-class AuthorAccounting(admin.ModelAdmin):
+class AuthorAccountingCashPaymentBalance(admin.ModelAdmin):
     list_display = ('cashPayment_balance_no','month','balance_cashPayment_close', 'balance_cashPayment_open', 'exchange_rate_close', 'exchange_rate_open', )
     def month(self, obj):
         return datetime.datetime.strptime(obj.cashPayment_balance_no, '%Y%m')
-admin.site.register(cashPaymentBalance, AuthorAccounting)
-admin.site.register(cashPayment)
+
+class AuthorAccountingCashPayment(admin.ModelAdmin):
+    search_fields = ['ticket_no', 'remark', 'settle', 'adv',]
+    list_display = ('ticket_no', 'created_at','updated_at','remark', 'settle', 'adv', 'debit', 'kredit', )
+
+    def debit(self, obj):
+        if(obj.is_debit):
+            debit = obj.rp_total
+        else:
+            debit = 0
+        return debit
+    
+    def kredit(self, obj):
+        if(obj.is_credit):
+            kredit = obj.rp_total
+        else:
+            kredit = 0
+        return kredit
+    
+admin.site.register(cashPaymentBalance, AuthorAccountingCashPaymentBalance)
+admin.site.register(cashPayment, AuthorAccountingCashPayment)
 admin.site.register(cashPaymentApprovalCashier)
